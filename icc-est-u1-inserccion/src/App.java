34b8c14cd1f29558ebee2e9ebd1319da67d73483
Nicolas <nico01pomaquiza@gmail.com>
2024-10-19 14:55:35 -0500

    TERMINADO

---
+++ icc-est-u1-inserccion/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        MetodoOrdenamiento ordenar = new MetodoOrdenamiento();
+        int[] arreglo = { 3, 5, 1, 2, 4 };
+        int[] arregloOrdenado = ordenar.sortInsertion(arreglo, true);
+        ordenar.printArray(arregloOrdenado);
+    }
+}
